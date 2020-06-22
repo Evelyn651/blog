1c58264dd449eda723fcb9b4d340af85f0253a13
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-06-22 11:19:52 -0500

    feat: finished the first two parts of the exercise

---
+++ src/main/java/com/codeup/blog/controllers/MathController.java
@@ -0,0 +1,31 @@
+package com.codeup.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MathController {
+    @GetMapping(path = "/add/{numberOne}/and/{numberTwo}")
+    @ResponseBody
+    public String add(@PathVariable double numberOne, @PathVariable double numberTwo) {
+        return numberOne + " + " + numberTwo + " = " + (numberOne + numberTwo) ;
+    }
+
+    @GetMapping(path = "/subtract/{numberOne}/and/{numberTwo}")
+    @ResponseBody
+    public String subtract(@PathVariable double numberOne, @PathVariable double numberTwo){
+        return numberOne + " - " + numberTwo + " = " + (numberOne - numberTwo);
+    }
+
+    @GetMapping(path = "/multiply/{numberOne}/and/{numberTwo}")
+    @ResponseBody
+    public String multiply(@PathVariable double numberOne, @PathVariable double numberTwo){
+        return numberOne + " * " + numberTwo + " = " + (numberOne * numberTwo);
+    }
+
+    @GetMapping(path = "/divide/{numberOne}/and/{numberTwo}")
+    @ResponseBody
+    public String divide(@PathVariable double numberOne,  @PathVariable double numberTwo){
+       return numberOne + " / " + numberTwo + " = " + (numberOne / numberTwo);
+    }
+}
